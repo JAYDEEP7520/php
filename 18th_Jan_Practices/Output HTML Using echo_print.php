503e4645e94a170eb542567e86baaf271e63eaa3
JAYDEEP <jppandya1999@gmail.com>
2020-01-20 14:44:30 +0530

    Delete for modifications..

--- 18th_Jan_Practices/Output HTML Using echo_print.php
+++
@@ -1,5 +0,0 @@
-<?php
-    // Displaying HTML Element by echo and print
-    echo '<input type = "text" name = "text" placeholder = "JAYDEEP .A. PANDYA">'.'<br>'.'<br>';
-    echo '<input type = "button" name = "submit" value = "EXAMPLE" disabled = "true">';
-?>
--- 18th_Jan_Practices/PHP Switch Statement.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $boolean = "true";
-    switch($boolean) {
-      case "true" : echo date("l jS \of F Y h:i:s A"); break;
-      case "false" : echo "False"; break;
-      default : echo "Not Match";
-  }
-?>
