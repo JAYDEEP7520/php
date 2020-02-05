cdd0424527f1bc5d7a512b844e99653884b1480b
JAYDEEP <jppandya1999@gmail.com>
2020-02-05 17:48:14 +0530

    HTML Code of Login Form and Registration Form

---
+++ 3rd_Feb_Evalution_TEST/Login_Form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Login Form </title>
+        <style>
+            #input { display: block; }
+        </style> 
+    </head>
+    <body>
+        <h1 align="center"> LOGIN </h1>
+        <div align="center">
+        <form>
+            Email <input id="input" type="text" name="email"/>
+            Password <input id="input" type="password" name="password"/>
+            <input type="button" name="login" value="LOGIN"/>
+            <input type="button" name="register" value="REGISTER"/>
+        </form>
+        <div>
+    </body>
+</html>
---
+++ 3rd_Feb_Evalution_TEST/Registration_Form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Regitration Form </title>
+        <style>
+            #input { display: block; }
+        </style> 
+    </head>
+    <body>
+        <h1 align="center"> REGISTER </h1>
+        <div align="center">
+        <form>
+            Select Prefix:<select name="prefix" id="input">
+                    <option value="Mr."> Mr. </option>
+                    <option value="Miss."> Miss. </option>
+                    <option value="Ms."> Ms. </option>
+                    <option value="Mrs."> Mrs. </option>
+                    <option value="Dr."> Dr. </option>
+                </select>
+            First Name <input id="input" type="text" name="firstname"/>
+            Last Name <input id="input" type="text" name="lastname"/>
+            Email <input id="input" type="email" name="email"/>
+            Mobile Number <input id="input" type="number" name="mobilenumber"/>
+            Password <input id="input" type="password" name="password"/>
+            Confirm Password <input id="input" type="password" name="confirmpassword"/>
+            Information <textarea rows="5" cols="30" name="information"> </textarea>
+            <input id="input" type="button" name="register" value="REGISTER"/>
+        </form>
+        <div>
+    </body>
+</html>
